99f3601f3ef0c0e268594852d4df3fe6830be2c2
Ibrahim Halit <104463962+ibo-ibo@users.noreply.github.com>
2022-12-02 02:55:10 +0300

    Update selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -1,7 +1,9 @@
 function selectionSort(arr) {
+  
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   };
+  
   for (let i = 0; i < arr.length; i++) {
     let lowest = i;
 
