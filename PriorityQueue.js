3dedfd8c5bed58ae57032449e1d967c61b222f12
Ibrahim Halit <104463962+ibo-ibo@users.noreply.github.com>
2022-12-05 07:44:02 +0300

    Create PriorityQueue.js

---
+++ PriorityQueue.js
@@ -0,0 +1,77 @@
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(val, priority) {
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
+
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      const parent = this.values[parentIdx];
+
+      if (element.priority >= parent.priority) break;
+
+      this.swap(idx, parentIdx);
+      idx = parentIdx;
+    }
+  }
+
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      let idx = 0;
+
+      const length = this.values.length;
+      const element = this.values[0];
+
+      while (true) {
+        let leftChildIdx = 2 * idx + 1;
+        let rightChildIdx = 2 * idx + 2;
+        let leftChild, rightChild;
+        let swap = null;
+
+        if (leftChildIdx < length) {
+          leftChild = this.values[leftChildIdx];
+          if (leftChild.priority < element.priority) {
+            swap = leftChildIdx;
+          }
+        }
+
+        if (rightChildIdx < length) {
+          rightChild = this.values[rightChildIdx];
+          if (
+            (swap === null && rightChild.priority < element.priority) ||
+            (swap !== null && rightChild.priority < leftChild.priority)
+          ) {
+            swap = rightChildIdx;
+          }
+        }
+
+        if (swap === null) break;
+
+        this.swap(swap, idx);
+        idx = swap;
+      }
+    }
+    return min;
+  }
+
+  swap(idx1, idx2) {
+    [this.values[idx1], this.values[idx2]] = [this.values[idx2], this.values[idx1]];
+  }
+}
